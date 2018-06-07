a1af6dc77fe9a982935ff42650b2f0e384513244
Syed Raza <razaphd@gmail.com>
2018-06-07 13:35:46 -0400

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World-1</p>'; ?> 
+ <?php echo '<p>Hello World-1. This is my firts PHP page using OpenShift Online</p>'; ?> 
  </body>
 </html>
