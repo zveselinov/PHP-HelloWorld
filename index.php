b4f55a51a9b791f71a7374b9f7ca8dac28c0043b
Syed Raza <razaphd@gmail.com>
2018-06-09 07:10:41 -0400

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World88888888888888. This is my first PHP100 page using OpenShift Online</p>'; ?> 
+ <?php echo '<p>Hello World-Happy Learning. This is my first Jenkins PHP200 page using OpenShift Online</p>'; ?> 
  </body>
 </html>
